bb369696fd72f546d4d57389a538d4c4d5b3c59a
AlexanderAlcazar <alcazaralexander46@outlook.com>
2026-07-29 11:37:37 -0700

    Add initial test reports and class files for BookRepositoryTest and AppTest
    - Created test report for BookRepositoryTest indicating an error due to a missing database driver.
    - Added compiled class files for AppTest and BookRepositoryTest.

---
+++ src/test/java/com/a2/repository/BookRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.a2.repository;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.a2.entity.Book;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class BookRepositoryTest {
+    @Autowired
+    private BookRepository bookRepository;
+    @Test
+    void testSearchBooks() {
+        List<Book> books = bookRepository.searchBooks("algorithms");
+        assertTrue(books.size() > 0);
+
+        for (Book book : books) {
+            System.out.println(">>> Book Title: " + book.getTitle());
+        }
+    }
+}
