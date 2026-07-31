a18362eb03ee48cfb45222e50c6f86182bb61c53
AlexanderAlcazar <alcazaralexander46@outlook.com>
2026-07-31 11:36:15 -0700

    Add book search controller and controller unit test

---
+++ src/main/java/com/a2/controller/BookController.java
@@ -0,0 +1,26 @@
+package com.a2.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.a2.entity.Book;
+import com.a2.service.BookService;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping("/search")
+    public List<Book> searchBooks(@RequestParam String searchTerm) {
+        return bookService.searchBooks(searchTerm);
+    }
+    
+}
